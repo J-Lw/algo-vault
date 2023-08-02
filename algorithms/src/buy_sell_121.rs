d45e9748e19a6e9c24308b866a81a0fa70568a7d
J-Lw <JLaw0@proton.me>
2023-08-02 06:49:07 -0300

    Algorithm checkpoint.

---
+++ algorithms/src/buy_sell_121.rs
@@ -0,0 +1,8 @@
+// Given an array *prices* where prices[i] is the price of a given stock on ith day, return the maximum profit that can be achieved by:
+// Choosing a single day to buy one stock and choosing a different day in the future to sell the stock.
+// If profit cannot be achieved, return 0.
+// Constraints: 1 <= prices.length <= 10exp5 | 0 <= prices[i] <= 10exp4.
+
+pub fn max_profit(prices: Vec<i32>) -> i32 {
+    
+}
