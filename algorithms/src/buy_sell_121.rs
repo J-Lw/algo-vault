a9135b80cd0b9dacd75135e7ef8e04993fa3dbba
J-Lw <JLaw0@proton.me>
2023-08-03 06:13:06 -0300

    Buy sell checkpoint.

--- algorithms/src/buy_sell_121.rs
+++ algorithms/src/buy_sell_121.rs
@@ -6,3 +6,20 @@
 pub fn max_profit(prices: Vec<i32>) -> i32 {
     
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_max_profit() {
+        // Single day, no transaction possible.
+        assert_eq!(max_profit(vec![8]), 0);
+
+        // Descending prices, profit not achievable.
+        assert_eq!(max_profit(vec![9, 8, 7, 6, 1]), 0);
+
+        // Buy on day 2 (price = 1) and sell on day 5 price = 6, profit = 6-1 = 5.]
+        assert_eq!(max_profit(vec![7, 1, 5, 3, 6, 4]), 5);
+    }
+}
--- algorithms/src/lib.rs
+++ algorithms/src/lib.rs
@@ -1,1 +1,2 @@
 pub mod majority_element_161;
+pub mod buy_sell_121;
