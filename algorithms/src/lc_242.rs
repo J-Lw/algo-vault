f5892c284e603cad4c909779f6ce434c583a6426
J-Lw <JLaw0@proton.me>
2023-08-31 09:00:50 -0300

    Completed Leetcode 242.

--- algorithms/src/lc_242.rs
+++ algorithms/src/lc_242.rs
@@ -13,17 +13,40 @@
 /// # Complexity:
 /// - Time: O(n).
 /// - Space: O(1).
+///
+/// # Intuition:
+/// - Employ frequency counting to determine if strings `s` and `t` have the same number of each character.
+/// - Increment counts for characters in `s` and decrement for `t`.
+/// - If all character frequencies zero out, `s` and `t` are anagrams.
 pub fn is_anagram(s: String, t: String) -> bool {
     if s.len() != t.len() {
         return false; // `s` and `t` cannot be anagrams if their lengths are not equivalent.
     }
 
-    let mut occurences = vec![0; 26]; // Init 26 element vector with 0 values to store the frequency of each character.
+    let mut occurrences = vec![0; 26]; // Init 26 element vector with 0 values to store the frequency of each character.
 
-    for (char_from_s, char_from_t) in s.chars().zip(t.chars()) { // Pair `s` and `t`'s characters into two element tuples and iterate over each tuple pair.
-        occurences[(char_from_s as u8 - b'a') as usize] += 1; // Cast char to its ASCII value, calculate the ASCII value difference between the char and 'a', cast the difference to usize to allow for vector indexing, and increment the count of char in `occurences` by 1.
-        occurences[(char_from_t as u8 - b'a') as usize] -= 1; // Decrement the count of char in `occurences` by 1, such that if `t` is an anagram of `s`, the count each char in `occurences` should balance to 0 via equal increments and decrements.
+    for (char_from_s, char_from_t) in s.chars().zip(t.chars()) {
+        // Pair `s` and `t`'s characters into two element tuples and iterate over each tuple pair.
+        occurrences[(char_from_s as u8 - b'a') as usize] += 1; // Cast char to its ASCII value, calculate the ASCII value difference between the char and 'a', cast the difference to usize to allow for vector indexing, and increment the count of char in `occurences` by 1.
+        occurrences[(char_from_t as u8 - b'a') as usize] -= 1; // Decrement the count of char in `occurrences` by 1, such that if `t` is an anagram of `s`, the count each char in `occurrences` should balance to 0 via equal increments and decrements.
     }
 
-    occurences.iter().all(|&element_value| element_value == 0) // Iterate over each element in the `occurences` collection and use dereferencing to unpack each element from a reference to a direct value to enable a zero check.
+    occurrences.iter().all(|&element_value| element_value == 0) // Iterate over each element in the `occurrences` collection and use dereferencing to unpack each element from a reference to a direct value to enable a zero check.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::is_anagram;
+
+    #[test]
+    fn test_is_anagram() {
+        // Not anagrams: different lengths.
+        assert!(!is_anagram("xyz".to_string(), "zyxz".to_string()));
+
+        // Anagrams.
+        assert!(is_anagram("silent".to_string(), "listen".to_string()));
+
+        // Not anagrams: same lengths.
+        assert!(!is_anagram("triangle".to_string(), "integral".to_string()));
+    }
 }
